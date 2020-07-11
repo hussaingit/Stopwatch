6e295f64668eb87e7216520a639b67ec9af32f99
hussaingit <newgreatgame514@gmail.com>
2020-07-11 15:19:44 +0500

    stopwatch

---
+++ app.js
@@ -0,0 +1,39 @@
+var min = 0;
+var sec= 0;
+var msec= 0;
+var minHead = document.getElementById("min");
+var secHead = document.getElementById("sec");
+var msecHead = document.getElementById("msec");
+var interval;
+function timer(){
+    msec++
+    msecHead.innerHTML=msec;
+    if(msec>=100){
+        sec++
+        secHead.innerHTML=sec;
+        msec=0;
+    }
+        else if (sec>=60){
+            min++
+            minHead.innerHTML=min;
+            sec = 0;
+
+        }
+    }
+
+function start(){
+interval=setInterval(timer,10)
+}
+
+function stop(){
+    clearInterval(interval)
+}
+function reset(){
+    min=0;
+    sec=0;
+    msec=0;
+    minHead.innerHTML= min;
+    secHead.innerHTML= sec;
+    msecHead.innerHTML= msec
+    stop();
+}
